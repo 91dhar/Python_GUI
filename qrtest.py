6b604c3ce85032e73563a14500a00526844e036f
Abhishek Dhar <68233166+Codeblocc@users.noreply.github.com>
2022-08-21 17:53:18 +0530

    Update qrtest.py

--- qrtest.py
+++ qrtest.py
@@ -1,12 +1,12 @@
 import qrcode
 from PIL import Image
 
-img = qrcode.make('https://www.youtube.com/watch?v=bPUXpH5Sww4&ab_channel=NBA')
+img = qrcode.make('https://streamable.com/io6r80')
 
 qrcode=qrcode.QRCode(version=1,
                      error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=4)
 
-qrcode.add_data('https://www.youtube.com/watch?v=bPUXpH5Sww4&ab_channel=NBA')
+qrcode.add_data('https://streamable.com/io6r80')
 qrcode.make(fit=True)
 img=qrcode.make_image(fill_color='green',back_color='white')
 img.save('TatumBuzzerBeater.png')
